2dbd689f6a0d8900b1d5904a48b995c38ac537ca
Xing Zhou <xing@xingzhou.me>
2012-09-02 11:43:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhi::Application.config.secret_token = '69d556873082ee80847f76d982e6b7bdf5ca33198134f6e5757904ab359b2b0f57c2412b6d06d23a1c30f79d2f057cba033a5bb5c3ae52f91577e78b9050eaff'
